01a94ebc83b8e94d73a9a35dcd087876efac8fbc
SRE-NADH <95708236+SRE-NADH@users.noreply.github.com>
2025-06-24 00:41:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+let handlePromise = (age) => {
+	return new Promise((resolve, reject) => {
+		if (Number(age) >= 18) {
+			setTimeout(() => {
+				
+				resolve("You can vote.");
+			}, 4000);
+		} else {
+			setTimeout(() => {
+				reject("You aren't old enough.");
+			}, 4000);
+		}
+	});
+};
+
+let handleSubmit = () => {
+	const age = document.getElementById('age').value;
+	const name = document.getElementById('name').value;
+
+	if (!age || !name) {
+		window.alert("Please enter valid details");
+		return;
+	}
+
+	handlePromise(age)
+		.then((message) => {
+			window.alert(message);
+		})
+		.catch((errorMessage) => {
+			window.alert(errorMessage);
+		});
+};
